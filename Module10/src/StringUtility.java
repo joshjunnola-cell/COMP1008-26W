1e8d55c14208dae406e90d2378bcaa03fd8c3c5f
Josh Junnola <joshjunnola@gmail.com>
2026-03-18 10:00:16 -0400

    completed bank account class module 10

---
+++ Module10/src/StringUtility.java
@@ -0,0 +1,26 @@
+// Fill in the class and methods
+public class StringUtility {
+
+
+    public String reverse(String str){
+        if(str == null) {
+            throw new IllegalArgumentException("String cannot be null")
+        }
+        String reversed = "";
+
+        for(int i = str.length()-1; i>=0; i--){
+            reversed += str.charAt((i));
+        }
+        return reversed;
+    }
+
+
+    public boolean isPalindrome(String str){
+        if(str == null) {
+            throw new IllegalArgumentException("String cannot be null");
+        }
+        String reversed = reverse(str);
+
+        return  str.equalsIgnoreCase(reversed);
+    }
+}
