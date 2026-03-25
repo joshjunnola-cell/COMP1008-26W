7f9f2af4f63cb97c5d552440d5e2dcbbecadf04d
Josh Junnola <joshjunnola@gmail.com>
2026-03-25 18:17:05 -0400

    lesson complete

--- Module11/JavaFXLearningActivity.java
+++ Module11/JavaFXLearningActivity.java
@@ -50,21 +50,20 @@ public class JavaFXLearningActivity extends Application {
         // TextField for name input
         // Button: "Submit"
         // Label for output
-        
+        Label nameLabel = new Label("Enter your name: ");
+        TextField nameField = new TextField ();
+        Button submitBtn = new Button("Submit");
+        Label outputLabel = new Label();
  
  
         // STEP 5:
         // Set button action:
         // When clicked, display:
         // "Hello <name>" in output label
- 
- 
- 
- 
- 
- 
- 
- 
+        // submitBtn.setOnAction(e -> {
+        //     String name = nameField.getText();
+        //     outputLabel.setText("Hello " + name);
+        // });
  
  
         // =====================================================
@@ -72,44 +71,29 @@ public class JavaFXLearningActivity extends Application {
         // Layouts (Containers)
         // =====================================================
  
- 
-        // STEP 6:
-        // Create a VBox layout with spacing 10
-        // Add all controls into VBox
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-        // STEP 7:
+
+                // STEP 7:
         // Create an HBox for buttons (if needed)
         // Add button inside HBox
- 
- 
- 
- 
- 
- 
- 
- 
- 
+        
+        HBox buttonBox = new HBox(10);
+        buttonBox.getChildren().add(submitBtn);
  
         // STEP 8:
         // Create a GridPane for form:
         // Add "Enter Name" label and TextField
- 
- 
- 
- 
- 
- 
- 
- 
+        
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.add(nameLabel, 0, 0);
+        grid.add(nameField, 1, 0);
+
+        // STEP 6:
+        // Create a VBox layout with spacing 10
+        // Add all controls into VBox
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(title, grid, buttonBox, outputLabel);
  
  
         // =====================================================
@@ -122,16 +106,18 @@ public class JavaFXLearningActivity extends Application {
         // Modify button click:
         // If input is empty → show "Name cannot be empty"
         // Else → show greeting message
+                submitBtn.setOnAction(e -> {
+            String name = nameField.getText();
+
+            if(name.isEmpty()){
+                outputLabel.setText("Name cannot be empty");
+
+            }else{
+                outputLabel.setText("Hello " + name);
+            }
+        });
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
         // =====================================================
         // ================= PART 5 ============================
         // MVC Concept (Understanding)
@@ -144,14 +130,7 @@ public class JavaFXLearningActivity extends Application {
         // Controller → (Which part handles click)
         // Model → (Where data is stored)
  
- 
- 
- 
- 
- 
- 
- 
- 
+        
  
         // =====================================================
         // ================= PART 6 ============================
@@ -162,27 +141,13 @@ public class JavaFXLearningActivity extends Application {
         // STEP 11:
         // Create a Scene using VBox layout
         // Set size 350 x 250
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+        Scene scene2 = new Scene(layout, 350, 250);
  
         // STEP 12:
         // Set Scene to Stage and display it
  
- 
- 
- 
- 
- 
- 
- 
- 
+        stage.setScene(scene2);
  
     }
  
